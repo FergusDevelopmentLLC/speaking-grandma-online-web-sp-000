f43570476aff1d270d7a136331eb013af612718e
Will Carter <will.carter@fergusllc.com>
2019-12-18 16:54:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,7 +9,7 @@ def speak_to_grandma(str)
   end
 end
 
-speak_to_grandma("hello")
+speak_to_grandma("HELLO")
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
